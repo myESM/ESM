e7c40584a7ccfb548e092cb5a2735dc48d9477da
csti-chenghanlin <chenghanlin@cyber00rn.org>
2020-08-25 02:30:39 +0000

    Revert "調整 Production API 網址"
    This reverts commit f3accffa1473470dd09d938b85c4b93db8eb16eb.

--- SecBuzzerESM/Crontab/scripts/Suricata_updater.sh
+++ SecBuzzerESM/Crontab/scripts/Suricata_updater.sh
@@ -2,12 +2,12 @@
 
 sleep $[ ( $RANDOM % 15 ) + 1 ]s
 ESM_API=`cat /.env | grep API_KEY_VALUE | cut -d = -f 2`
-current_rules_version=`curl -X POST "https://api.hub.secbuzzer.co/esmapi/web/file/fileVersion" -H "accept: */*" -H "authorization: $ESM_API" | cut -d : -f 2 | cut -d \" -f 2`
+current_rules_version=`curl -X POST "https://api.secbuzzer.co/esmapi/web/file/fileVersion" -H "accept: */*" -H "authorization: $ESM_API" | cut -d : -f 2 | cut -d \" -f 2`
 local_rules_version=$(</tmp/local_rules_version)
 if [ "$current_rules_version" != "$local_rules_version" ]
 then
     echo "[*] New version found! rules update to $current_rules_version"
-    curl -o /tmp/rules.tgz "https://api.hub.secbuzzer.co/esmapi/web/file/download/$current_rules_version" -H "accept: */*" -H "authorization: $ESM_API"
+    curl -o /tmp/rules.tgz "https://api.secbuzzer.co/esmapi/web/file/download/$current_rules_version" -H "accept: */*" -H "authorization: $ESM_API"
     mkdir -p /tmp/rules
     tar zxf /tmp/rules.tgz -C /tmp/rules
     chown 1000:1000 /tmp/* -R
--- SecBuzzerESM/Install.sh
+++ SecBuzzerESM/Install.sh
@@ -66,8 +66,8 @@ ln -s ../SecBuzzerESM.env AI/.env 2>/dev/nul
 
 API_KEY=`cat SecBuzzerESM.env | grep API_KEY_VALUE | cut -d = -f 2`
 mkdir -p tmp/rules
-rules_version=`curl -X POST "https://api.hub.secbuzzer.co/esmapi/web/file/fileVersion" -H "accept: */*" -H "authorization: $API_KEY" | cut -d : -f 2 | cut -d \" -f 2`
-curl -o rules.tgz "https://api.hub.secbuzzer.co/esmapi/web/file/download/$rules_version" -H "accept: */*" -H "authorization: $API_KEY"
+rules_version=`curl -X POST "https://api.secbuzzer.co/esmapi/web/file/fileVersion" -H "accept: */*" -H "authorization: $API_KEY" | cut -d : -f 2 | cut -d \" -f 2`
+curl -o rules.tgz "https://api.secbuzzer.co/esmapi/web/file/download/$rules_version" -H "accept: */*" -H "authorization: $API_KEY"
 tar zxvf rules.tgz -C tmp/rules
 sudo chown 1000:1000 /tmp/* -R
 sudo rsync -r --delete tmp/rules/ Suricata/suricata/rules/
