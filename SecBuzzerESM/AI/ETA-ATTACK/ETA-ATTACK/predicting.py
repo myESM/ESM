f76917ab4bdf9bf794f510f0a238319b75886c8f
kwlee12 <kwlee@cyber00rn.org>
2020-08-27 17:10:41 +0800

    adjust column name and file name

---
+++ SecBuzzerESM/AI/ETA-ATTACK/ETA-ATTACK/predicting.py
@@ -0,0 +1,138 @@
+import datetime
+import configparser, pickle, json
+import pandas as pd
+import numpy as np
+
+from elasticsearch import Elasticsearch, helpers
+
+
+class Main():
+  def __init__(self):
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    # es
+    self.es_server = config.get("ES", "es_server")
+    self.es_port = eval(config.get("ES", "es_port"))
+    # model
+    self.K = eval(config.get("Model", "K"))
+    self.N_Class = eval(config.get("Model", "N_Class"))
+    self.model_path = config.get("Model", "model_path")
+    self.m0 = config.get("Model", "m0")
+    self.m1 = config.get("Model", "m1")
+    self.m2 = config.get("Model", "m2")
+    self.m3 = config.get("Model", "m3")
+    self.m4 = config.get("Model", "m4")
+    self.preds = eval(config.get("Model", "preds"))
+    self.attck_table = eval(config.get("Model", "attck_table"))
+    self.attck_msg = eval(config.get("Model", "attck_msg"))
+    self.proto_table = eval(config.get("Model", "proto_table"))
+    self.sig_id = eval(config.get("Model", "sig_id"))
+    self.url_table = eval(config.get("Model", "url_table"))
+
+  def queryES(self, es_index, start_time, end_time):
+    es = Elasticsearch([{"host":self.es_server, "port":self.es_port}])
+    print("start_time:", start_time)
+    print("end_time:", end_time)
+    body = {"query":{"bool":{"must":[{"range":{"Timestamp":{"gt":start_time,"lt":end_time}}}]}}}
+    res = helpers.scan(client=es, scroll="2m", query=body, index=es_index)
+    print("es_index:", es_index)
+    return res
+  
+  def toDf(self, res):
+    data = list()
+    for nf in res:
+      tmp = dict()
+      tmp["id"] = nf["_id"]
+      tmp.update(nf["_source"])
+      data.append(tmp)
+    df = pd.DataFrame(data)
+    return df
+  
+  def processData(self, df):
+    # 67 features and rename columns
+    df_fin = df[self.preds]
+    df_id = df[["id", "Timestamp", "Src IP", "Src Port", "Dst IP", "Dst Port", "Protocol", "nic_name"]]
+    return df_fin, df_id
+  
+  def predict(self, df):
+    m0 = pickle.load(open(self.model_path+self.m0, "rb"))
+    m1 = pickle.load(open(self.model_path+self.m1, "rb"))
+    m2 = pickle.load(open(self.model_path+self.m2, "rb"))
+    m3 = pickle.load(open(self.model_path+self.m3, "rb"))
+    m4 = pickle.load(open(self.model_path+self.m4, "rb"))
+    predictions = np.zeros([self.K, len(df), self.N_Class])
+    # predict
+    predictions[0] = m0.predict_proba(df.values)
+    predictions[1] = m1.predict_proba(df.values)
+    predictions[2] = m2.predict_proba(df.values)
+    predictions[3] = m3.predict_proba(df.values)
+    predictions[4] = m4.predict_proba(df.values)
+    #average
+    pred = np.argmax(predictions.mean(axis=0), axis=1)
+    return pred
+
+  def toJSON(self, pred, df_id):
+    df = df_id.copy()
+    # rename
+    df.rename(columns={"id":"flow_id",
+                       "Timestamp":"log_time",
+                       "Src IP":"src_ip",
+                       "Src Port":"src_port",
+                       "Dst IP":"dest_ip",
+                       "Dst Port":"dest_port",
+                       "Protocol":"proto",
+                       "nic_name":"in_iface"}, inplace=True)
+    df["src_port"] = df["src_port"].astype("int")
+    df["dest_port"] = df["dest_port"].astype("int")
+    df["proto"] = df["proto"].apply(lambda x: self.proto_table[x])
+    df["title"] = [self.attck_table[x] for x in pred] # alert.category
+    t = datetime.datetime.now()
+    utc = datetime.datetime.now(tz=datetime.timezone.utc)
+    df["timestamp"] = t.strftime("%Y-%m-%dT%H:%M:%S.%f+08:00")
+    df["ingest_timestamp"] = utc.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    df["timestamp"] = t
+    df["event_type"] = "alert"
+    df["severity"] = 2 # alert.severity
+    df["message"] = df["title"].apply(lambda x: self.attck_msg[x]) # alert.signature
+    df["action"] = "" # alert.action
+    df["rule_sig_id"] = df["title"].apply(lambda x: self.sig_id[x]) # alert.signature_id
+    df["alert_group_id"] = 0 # alert.gid
+    df["alert"] = df.apply(lambda x: {"category":x["title"], "severity":x["severity"], "signature":x["message"],
+                                      "action":x["action"], "signature_id":x["rule_sig_id"], "gid":x["alert_group_id"]}, axis=1)
+    df["reference"] = df["title"].apply(lambda x: self.url_table[x])
+    df.drop(["title", "severity", "message", "action", "rule_sig_id", "alert_group_id"], axis=1, inplace=True)
+    df["module"] = "ETA-ATTACK"
+    df["log_type"] = "traffic"
+    df["dump_status"] = "0"
+    output = eval(df.to_json(orient="records"))
+    return output
+  
+  def toES(self, dic):
+    count = 0
+    es_idx = datetime.datetime.now().strftime("eta-attack-%Y-%m")
+    for d in dic:
+      if d["alert"]["category"] == "Benign":
+        pass
+      else:
+        count += 1
+        es = Elasticsearch([{"host":self.es_server, "port":self.es_port}])
+        es.index(index=es_idx, doc_type="_doc", body=d)
+    print("{} NetFlows are anomaly and save to ES".format(count))
+
+
+if __name__ == "__main__":
+  es_index = "cic_20200804"
+  start_time = "2020-08-04T11:25:00.000000+08:00"
+  end_time = "2020-08-04T11:30:00.000000+08:00"
+
+  load_main = Main()
+  res = load_main.queryES(es_index, start_time, end_time)
+  df = load_main.toDf(res)
+  if df.shape[0] != 0:
+    df_fin, df_id = load_main.processData(df)
+    pred = load_main.predict(df_fin)
+    output = load_main.toJSON(pred, df_id)
+    load_main.toES(output)
+    print("[DONE]")
+  else:
+    print("[MSG] There is no data")
---
+++ SecBuzzerESM/AI/ETA-ATTACK/ETA-ATTACK/readme.md
@@ -0,0 +1,163 @@
+# ETA1
+
+## Prerequest
+
+An `Python3` environment. If you want to run it on docker, recommand using `python:3.8.5-buster`(gcc is needed).
+
+## Install library with pip
+You can use `pip3 install` to install packages below
+
+- wheel==0.31.1
+- Cython==0.28.2
+- elasticsearch==6.3.1
+- Flask==1.0.2
+- xgboost==1.0.0
+- scikit-learn==0.23.1
+- pandas==0.25.0
+- numpy==1.18.4
+
+or you can use `pip3 install -r requirements.txt`
+
+[NOTE] There may be an error message for build pandas, but it doesn't matter.
+
+## config.ini
+
+Parameters listed below could be changed with requirement.
+
+```
+[ES]
+es_server = 192.168.70.120
+es_port = 9201
+```
+
+## Run Code
+
+```
+python3 eta1_api.py
+```
+
+## API Example
+
+- API route: eta1/api/v1.0
+- API example
+	- index
+	- start_time
+		- format: YYYY-mm-ddTHH:MM:SS.ffffff+08:00
+	- end_time
+ 		- format: YYYY-mm-ddTHH:MM:SS.ffffff+08:00
+
+
+```
+http://localhost:5000/eta1/api/v1.0?index=cic_20200804&start_time=2020-08-04T11:25:00.000000%2B08:00&end_time=2020-08-04T11:30:00.000000%2B08:00
+```
+
+```
+curl -i http://localhost:5000/eta1/api/v1.0\?index\=cic_20200804\&start_time\=2020-08-04T11:25:00.000000%2B08:00\&end_time\=2020-08-04T11:30:00.00000%2B08:00
+```
+
+## Response Code
+Code | Description
+-----|--------------
+200  | Success
+404  | Not Found
+500  | Internal Server Error
+
+## Input Schema
+Below table is an example of input schema from ES.
+
+Key                         | Value
+----------------------------|---------------
+Src Port                    | 59175
+Dst Port                    | 9201
+Flow Duration               | 27843
+Total Fwd Packet            | 5
+Total Bwd packets           | 3
+Total Length of Fwd Packet  | 2683.0
+Total Length of Bwd Packet  | 323.0
+Fwd Packet Length Max       | 1460.0
+Fwd Packet Length Min       | 0.0
+Fwd Packet Length Mean      | 536.6
+Fwd Packet Length Std       | 739.5321494025801
+Bwd Packet Length Max       | 323.0
+Bwd Packet Length Min       | 0.0
+Bwd Packet Length Mean      | 107.66666666666667
+Bwd Packet Length Std       | 186.48413694824913
+Flow Bytes/s                | 107962.50404051288
+Flow Packets/s              | 287.32536005459184
+Flow IAT Mean               | 3977.5714285714284
+Flow IAT Std                | 6941.2158602832405
+Flow IAT Max                | 19513.0
+Flow IAT Min                | 24.0
+Fwd IAT Total               | 27843.0
+Fwd IAT Mean                | 6960.75
+Fwd IAT Std                 | 12053.697893869195
+Fwd IAT Max                 | 25006.0
+Fwd IAT Min                 | 24.0
+Bwd IAT Total               | 23218.0
+Bwd IAT Mean                | 11609.0
+Bwd IAT Std                 | 11177.943996996943
+Bwd IAT Max                 | 19513.0
+Bwd IAT Min                 | 3705.0
+Fwd PSH Flags               | 0
+Fwd URG Flags               | 0
+Fwd Header Length           | 112
+Bwd Header Length           | 72
+Fwd Packets/s               | 179.5783500341199
+Bwd Packets/s               | 107.74701002047193
+Packet Length Min           | 0.0
+Packet Length Max           | 1460.0
+Packet Length Mean          | 334.0
+Packet Length Std           | 583.915019501982
+Packet Length Variance      | 340956.74999999994
+FIN Flag Count              | 1
+SYN Flag Count              | 2
+PSH Flag Count              | 2
+ACK Flag Count              | 7
+URG Flag Count              | 0
+Down/Up Ratio               | 0.0
+Average Packet Size         | 375.75
+Fwd Segment Size Avg        | 536.6
+Bwd Segment Size Avg        | 107.66666666666667
+Subflow Fwd Packets         | 0
+Subflow Fwd Bytes           | 335
+Subflow Bwd Packets         | 0
+Subflow Bwd Bytes           | 40
+FWD Init Win Bytes          | 64240
+Bwd Init Win Bytes          | 501
+Fwd Act Data Pkts           | 2
+Fwd Seg Size Min            | 20
+Active Mean                 | 0
+Active Std                  | 0
+Active Max                  | 0
+Active Min                  | 0
+Idle Mean                   | 1.596511622913498E15
+Idle Std                    | 0.0
+Idle Max                    | 1.596511622913498E15
+Idle Min                    | 1.596511622913498E15
+
+## Output Schema
+Below table is an example of output schema to ES.
+
+Key                 | Value
+--------------------|---------------
+flow_id             | XyPZBXEBOI2XA3yCwRPD
+timestamp           | 2020-03-23T13:24:39.144408+08:00
+event_type          | alert
+alert.category      | Endpoint Denial of Service
+alert.severity      | 10
+alert.signature     | CSTI ETA1 - ATT&CK Endpoint Denial of Service - T1499
+src_ip              | 192.168.70.196
+dest_ip             | 192.168.70.1
+src_port            | 138
+dest_port           | 138
+alert.action        | N/A
+proto               | tcp
+module              | ETA1
+in_iface            | [From Input]
+alert.signature_id  | 20110002
+log_type            | NetFlow
+reference           | https://attack.mitre.org/techniques/T1204/
+log_time            | 2020-03-11T10:02:56.762791+08:00
+alert.gid           | 0
+dump_status         | 0
+ingest_timestamp    | 2020-03-23T05:24:39.144408Z
