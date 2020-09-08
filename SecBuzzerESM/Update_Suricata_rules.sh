5da0b15cc8efb31411d55eb643d4619eebe47aa9
csti-chenghanlin <chenghanlin@cyber00rn.org>
2020-09-08 04:20:20 +0000

    Upgrade_ESM 安裝模式參數 Upgrade_ESM 移除無用(none) Image Update_Suricata_rules 無 KEY 防呆不噴錯

--- SecBuzzerESM/Update_Suricata_rules.sh
+++ SecBuzzerESM/Update_Suricata_rules.sh
@@ -1,4 +1,6 @@
 API_KEY=`cat SecBuzzerESM.env | grep API_KEY_VALUE | cut -d = -f 2`
+if [ -n "$API_KEY" ]
+then
 mkdir -p tmp/rules
 current_rules_version=`curl -X POST "https://test.api.secbuzzer.co/esmapi/web/file/fileVersion" -d "{'TypeCode': 'it'}" -H "Content-Type: application/json" -H "accept: */*" -H "authorization: $API_KEY" | cut -d : -f 2 | cut -d \" -f 2`
 curl -o rules.tgz "https://test.api.secbuzzer.co/esmapi/web/file/download/it/$current_rules_version" -H "accept: */*" -H "authorization: $API_KEY"
@@ -7,3 +9,6 @@ sudo chown 1000:1000 /tmp/* -R
 sudo rsync -r --delete tmp/rules/ Suricata/suricata/rules/
 rm -rf tmp rules.tgz
 docker-compose --env-file SecBuzzerESM.env -f Suricata/docker-compose.yml restart
+else
+echo No API key found, check SecBuzzerESM.env
+fi
--- SecBuzzerESM/Upgrade_ESM.py
+++ SecBuzzerESM/Upgrade_ESM.py
@@ -8,8 +8,15 @@ import sys
 parser = ArgumentParser()
 parser.add_argument("-p", "--path", help="ESM's path", dest="ESM_Path", default="/opt/SecbuzzerESM")
 parser.add_argument("-C", "--cache", help="Install with cache", action="store_true")
+parser.add_argument("-F", "--full", help="Full upgrade, remove all images and run Install.sh", action="store_true")
+parser.add_argument("-M", "--medium", help="Medium upgrade, rebuild images and pull images", action="store_true")
+parser.add_argument("-S", "--small", help="Small upgrade, just update files", action="store_true")
 args = parser.parse_args()
 ESMPATH = args.ESM_Path
+FULL_UPGRADE = args.full
+MEDIUM_UPGRADE = args.medium
+SMALL_UPGRADE = args.small
+
 vers = None
 lastest_ver = None
 prev_ver = None
@@ -48,19 +55,19 @@ def init():
 
 def update():
     cache = '' if args.cache else '--no-cache'
-    if lastest_ver[0] != prev_ver[0]:
+    if lastest_ver[0] != prev_ver[0] or FULL_UPGRADE:
         # Exec Install.sh
         # rsync + esm.env
         # remove all images
         # sys.exit
         # 本次升級需重新設定 SecBuzzerESM.env
         subprocess.call(f'rsync -a --progress . {ESMPATH}', shell=True)
-        subprocess.call('docker rmi $(docker images -q) > /dev/null', shell=True)
+        subprocess.call('docker rmi -f $(docker images -q) > /dev/null', shell=True)
         subprocess.call('bash Install.sh', shell=True)
         tprint(f'{bcolors.OKGREEN}更新完成')
-        tprint(f'{bcolors.WARNING}本次升級需重新設定 SecBuzzerESM.env')
+        tprint(f'{bcolors.WARNING}本次升級需重新設定 SecBuzzerESM.env, 設定完畢後執行 Update_Suricata_rules.sh')
         sys.exit()
-    if lastest_ver[1] != prev_ver[1]:
+    if lastest_ver[1] != prev_ver[1] or MEDIUM_UPGRADE:
         # rsync
         # no-cache install all images
         # sys.exit
@@ -68,14 +75,16 @@ def update():
         for path in yamls:
             subprocess.call(f'docker-compose -f {path} build {cache}', shell=True)
             subprocess.call(f'docker-compose -f {path} pull', shell=True)
+        subprocess.call('docker rmi $(sudo docker images -f "dangling=true" -q) > /dev/null', shell=True)
         tprint(f'{bcolors.OKGREEN}更新完成')        
         sys.exit()
 
-    if lastest_ver[2] != prev_ver[2]:
+    if lastest_ver[2] != prev_ver[2] or SMALL_UPGRADE:
         subprocess.call(f'rsync -a --progress --exclude=SecBuzzerESM.env . {ESMPATH}', shell=True)
         tprint(f'{bcolors.OKGREEN}更新完成')
         sys.exit()
-    tprint(f'{bcolors.FAIL} 更新失敗, 請洽 ESM 工程師')
+
+    tprint(f'{bcolors.FAIL}更新失敗, 請洽 ESM 工程師')
     sys.exit(1)
     
 if __name__ == "__main__":
