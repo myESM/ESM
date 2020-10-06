65224912e4276384a661b086b0c8cfe835d84a9f
csti-chenghanlin <chenghanlin@cyber00rn.org>
2020-10-06 07:51:26 +0000

    移除 FluentdCron 去除 Fluentd 收 Log 功能

--- SecBuzzerESM/Fluentd/cron_image/daily.sh
+++
@@ -1,6 +0,0 @@
-#Variable setting
-createDay="$(date +'%Y-%m-%d' -d'+1 day')"
-deleteDay="$(date +'%Y-%m-%d' -d'-365 day')"
-
-#Delete ninety days ago index
-curl -X DELETE "localhost:19200/lm-$deleteDay*?pretty"
